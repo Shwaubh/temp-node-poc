609e6924b6f6445f241db7811b78e15d03f57fd2
Saurabh Kumar Giri <skgvns8789@gmail.com>
2022-03-06 06:56:10 +0530

    Added 1 item - 5 into items array

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const _ = require("lodash")
 
-const items = [1, [2, [3, 4]]]
+const items = [1, [2, [3, 4, 5]]]
 const new_items = _.flattenDeep(items)
 
 console.log('I am the starting point of this application');
